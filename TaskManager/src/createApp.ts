0d775b254068a9f5dc471b1b7b49faace2b699a1
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2026-01-20 20:10:59 +0700

    feat: update render yaml

--- TaskManager/src/createApp.ts
+++ TaskManager/src/createApp.ts
@@ -27,7 +27,7 @@ export const createApp = async () => {
   // Middlewares
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
       credentials: true,
       exposedHeaders: ["Authorization"],
     }),
