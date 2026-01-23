065b9b89b7ef4a8657ddcd3e3aac1bccdb1a093d
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2026-01-23 12:42:01 +0700

    fix: type error

--- frontend/src/components/projects/activity/IssueActivitySection.tsx
+++ frontend/src/components/projects/activity/IssueActivitySection.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useActivityStore } from "../../../stores/activityStore";
 import { Clock, RefreshCw, Activity } from "lucide-react";
